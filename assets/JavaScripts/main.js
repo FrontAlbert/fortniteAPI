ab9a527b3ecdd6cfb08ecda7419f408f371c9e62
FrontAlbert <alberthui@outlook.com>
2021-02-24 14:48:59 -0500

    changes

--- assets/JavaScripts/main.js
+++ assets/JavaScripts/main.js
@@ -12,7 +12,6 @@ async function getPlayer(gamerId) {
     return data;
 }
 
-
 // getBanners().then((res)=>{
 //     banners = res.data;
 //     console.log(banners)
@@ -33,24 +32,35 @@ const showData = () => {
         .then((res) => {
             const markup = `
         <div class="player-name">
-        <h1>${res.data.account.name}</h1>
-        <h4>${res.data.account.id} <h4>
+            <h1>${res.data.account.name}</h1>
+        <div class="player-header">
+            <p>Total Wins ${res.data.stats.all.overall.wins}</p>
+            <p>Win% ${res.data.stats.all.overall.winRate}</p>
+            <p>Kills  ${res.data.stats.all.overall.kills}</p>
+            <p>K/D  ${res.data.stats.all.overall.kd}</p>
+        </div>
         <div class="player">
-                <div class="player-wins">
-                    <h5>Total Wins: ${res.data.stats.all.overall.wins}</h5>
-                    <h5>Top 3 Placings: ${res.data.stats.all.overall.top3}</h5>
-                    <h5>Top 10 Placings: ${res.data.stats.all.overall.top10}</h5>
-                <div>
-                <div class="player-kills">
-                    <h5>KD Ratio: ${res.data.stats.all.overall.kd}</h5>
-                    <h5>Kills: ${res.data.stats.all.overall.kills}</h5>
-                    <h5>Kills Per Minute: ${res.data.stats.all.overall.killsPerMin}</h5>
-                <div>
-                <div class="player-ltm">
-                    <h5>KD Ratio: ${res.data.stats.all.overall.kd}</h5>
-                    <h5>Kills: ${res.data.stats.all.overall.kills}</h5>
-                    <h5>Kills Per Minute: ${res.data.stats.all.overall.killsPerMin}</h5>
-                <div>
+                <div class="solo-stats">
+                    <h2>Solos</h2>
+                    <p>Wins: ${res.data.stats.all.solo.wins}</p>
+                    <p>Win% ${res.data.stats.all.solo.winRate}</p>
+                    <p>K/D: ${res.data.stats.all.solo.kd}</p>
+                    <p>Top 10:  ${res.data.stats.all.solo.top10}</p>
+                </div>
+                <div class="duo-stats">
+                    <h2>Duos</h2>
+                    <p>Wins: ${res.data.stats.all.duo.wins}</p>
+                    <p>Win% ${res.data.stats.all.duo.winRate}</p>
+                    <p>K/D: ${res.data.stats.all.duo.kd}</p>
+                    <p>Top 5:  ${res.data.stats.all.duo.top5}</p>
+                </div>
+                <div class="quad-stats">
+                    <h2>Quads</h2>
+                    <p>Wins: ${res.data.stats.all.squad.wins}</p>
+                    <p>Win% ${res.data.stats.all.squad.winRate}</p>
+                    <p>K/D: ${res.data.stats.all.squad.kd}</p>
+                    <p>Top 3:  ${res.data.stats.all.squad.top3}</p>
+                </div>
             </div>
         </div>
         `;
