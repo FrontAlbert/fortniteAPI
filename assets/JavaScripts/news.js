e5eab994addadf4b7a68a247ab0f9b0687a18304
FrontAlbert <alberthui@outlook.com>
2021-02-24 17:48:34 -0500

    Added Daily shop

--- assets/JavaScripts/news.js
+++ assets/JavaScripts/news.js
@@ -1,26 +1,43 @@
-const Banners = document.querySelector(".banners");
+const dailyShop = document.querySelector(".dailyshop");
+const weeklyItems = document.createElement("li");
+const date = document.getElementById("date")
+
+let d = new Date();
+date.innerHTML = d;
+
 
 async function getBanners() {
-    const getData = await fetch(`https://fortnite-api.com/v1/banners`);
+    const getData = await fetch(`https://fortnite-api.com/v2/shop/br`);
     const data = await getData.json();
-    console.log(data);
     return data;
 }
 
 const showBanners = () => {
-    getBanners().then((res)=>{
-        banners = res.data;
-        console.log(banners)
-        banners.slice(1,34).forEach(x => {
-            const markup=`
-            <img src=${x.images.smallIcon}></img>
-            `;
-            Banners.insertAdjacentHTML("beforeend", markup);
-        })
-
-    })
+    getBanners().then((res) => {
+        banners = res.data.daily.entries;
+        banners.forEach((x) => {
+            console.log(x.items);
+            dailyItems = x.items;
+            dailyItems
+                .forEach((y) => {
+                    const markup=`
+                    <div class="entire">
+                        <div class="inner-entire">
+                            <img src=${y.images.smallIcon} class="daily-images">
+                            </img>
+                            <div class="details">
+                                    <p class="hover-text">${y.name}</p>
+                                    <p class="hover-text"> ${y.type.value}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                dailyShop.insertAdjacentHTML("beforeend", markup);
+                })
+        });
+    });
 };
 
 window.onload = (event) => {
-    showBanners()
+    showBanners();
 };
